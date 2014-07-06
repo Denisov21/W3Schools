e0cf226efbb167b6451cbf6dc71bb5442e6e3ea7
Denisov21 <student@alice.it>
2014-07-06 11:18:32 +0200

    v 1.1

--- main.js
+++ main.js
@@ -8,21 +8,21 @@ define(function (require, exports, module) {
         EditorManager  = brackets.getModule("editor/EditorManager"),
         Menus          = brackets.getModule("command/Menus");
 
-    function W3CShool() {
+    function W3CShools() {
         var thisEditor = EditorManager.getCurrentFullEditor();
         var query = thisEditor._codeMirror.getSelection();
         window.open("http://www.w3schools.com/" + encodeURIComponent(query));
     }
 
 
-    var W3C_SCHOOL = "Denisov.W3CSchool";
+    var W3C_SCHOOLS = "Denisov.W3CSchools";
 
-    CommandManager.register("W3C School Tutorial", W3C_SCHOOL, W3CShool);
+    CommandManager.register("W3C Schools Tutorial", W3C_SCHOOLS, W3CShools);
 
-    var fileMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
-    fileMenu.addMenuDivider();
-    fileMenu.addMenuItem(W3C_SCHOOL, "Ctrl-Alt-W");
-    contextMenu.addMenuDivider();
-    contextMenu.addMenuItem(W3C_SCHOOL, "Ctrl-Alt-W");
+    var helpMenu = Menus.getMenu(Menus.AppMenuBar.HELP_MENU);
+    /*var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);*/
+    helpMenu.addMenuDivider();
+    helpMenu.addMenuItem(W3C_SCHOOLS, "Ctrl-Alt-W");
+    /*contextMenu.addMenuDivider();
+    contextMenu.addMenuItem(W3C_SCHOOLS, "Ctrl-Alt-W");*/
 });
